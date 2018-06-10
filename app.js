21c7bfdad466b93aedad98fb1f9dbf8bb02b9ab8
ma223ik <ma223ik@gmail.com>
2018-06-10 21:09:59 +0200

    Working on the start page

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var app = function (){
   var questionButton = document.getElementById("stockName");
   var search = document.getElementById("searchButton");
-
-
+  var ceoPicture = document.getElementById("pic");
 	search.onclick = function () {
-
 	  if(questionButton.value == 'Apple'){
-	  	console.log('hej');
+		href.link ="Apple.html";
 	  }
 	}
 }
