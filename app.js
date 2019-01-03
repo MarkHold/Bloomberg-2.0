a735057b39ff5cff8a4edfeb3daf525368c4edeb
markhold <ma223ik@gmail.com>
2019-01-03 23:15:20 +0100

    Fixes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var app = function (){
-  var questionButton = document.getElementById("stockName");
-  var search = document.getElementById("searchButton");
-  var ceoPicture = document.getElementById("pic");
-	search.onclick = function () {
-	  if(questionButton.value == 'Apple'){
-		href.link ="Apple.html";
-	  }
-	}
-};
 
+var app = function() {
+
+        var ticker = document.getElementById("ticker");
+
+        //ticker.innerHTML = 'bruh';
+
+        $.getJSON("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=KOE7PMEOOPD18BYD", function (json) {
+
+          var times = json["Realtime Currency Exchange Rate"];
+
+              console.log(times[0]);
+    });
+};
 app();
